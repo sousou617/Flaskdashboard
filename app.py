ab27690a14d3f250407d76ef9d636d377cebc040
Cheryl Brown <sousou617@gmail.com>
2019-08-09 15:39:10 -0400

    Adding various html files

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-
+from flask_bootstrap import Bootstrap
+import matplotlib.pyplot as plt
+import numpy as np 
+import random
 from flask import Flask, render_template, url_for
+
 
 app = Flask(__name__)
 app.config['SERVER_NAME'] = 'localhost:5000'
-#with open('myfile.json') as fi:
-#	data = fi.readlines()
 
-@app.route('/home')
-def index():
-    return render_template('home.html')
+Bootstrap(app)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html', argument_dict)
 
 @app.route('/login')
 def about():
    return render_template('login.html')
 
 
-#     @app.route('/data/<string:id>/')
-# def SlaData(id):
-#     return render_template('SLAdata.html', id=id)
 
 with app.app_context():
 	url_for('static', filename='css/style.css')
